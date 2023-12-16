14aef571d520b09a9bd3e82b5bee9978085775a7
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-12-17 00:02:34 +0900

    feat: 여러 페이지를 동기적으로 가져오도록 변경

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,15 @@ import {configList} from "../config.js";
 import type {Config} from "./config.js";
 import {crawl, write} from "./core.js";
 
-Promise.all(
-  configList.map(async ({title, ...restConfig}) => {
-    const dateText = new Date().toISOString().split("T")[0];
-    const fullConfig: Config = {
-      ...restConfig,
-      maxPagesToCrawl: 99999,
-      outputFileName: `${title}-${dateText}.json`,
-    };
+for (const config of configList) {
+  const {title, ...restConfig} = config;
+  const dateText = new Date().toISOString().split("T")[0];
+  const fullConfig: Config = {
+    ...restConfig,
+    maxPagesToCrawl: 99999,
+    outputFileName: `${title}-${dateText}.json`,
+  };
 
-    await crawl(fullConfig);
-    await write(fullConfig);
-  })
-);
+  await crawl(fullConfig);
+  await write(fullConfig);
+}
